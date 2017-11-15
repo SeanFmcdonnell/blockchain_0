637b229bfefb17dbf12ac50c4511e8002c66b8b6
Sean McDonnell <sean@freshpow.io>
2017-11-15 11:47:38 -0500

    updates to the blockchain

--- main.js
+++ main.js
@@ -33,6 +33,21 @@ class Blockchain{
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid(){
+      for(let i = i; i < this.chain.length; i++){
+        const currentBlock = this.chain[i];
+        const previousBlock = this.chain[i - 1];
+
+        if(currentBlock.hash !== currentBlock.calculateHash()){
+          return false;
+        }
+        if(currentBlock.previousHash !== previousBlock.hash){
+          return false;
+        }
+      }
+      return true;
+  }
 }
 
 let seanCoin = new Blockchain();
